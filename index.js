ec4524e2bc2a2bb9c107b815778b6ece3683ca58
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-10-01 07:39:50 +0600

    Instructor json added

--- index.js
+++ index.js
@@ -6,11 +6,36 @@ const cors = require("cors");
 
 const port = process.env.PORT || 5050;
 
+const instructor = require("./JSON/instructor.json");
+const recipes = require("./JSON/course.json");
+
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.json({ message: "hello server running" })
-})
+// send all instructor
+app.get("/instructor", (req, res) => {
+    res.send(instructor);
+});
+
+
+app.get("/recipes", (req, res) => {
+    res.send(recipes);
+});
+
+
+app.get("/recipe/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedRecipe = recipe.find((recipe) => recipe.recipe_id === id);
+    res.send(selectedRecipe);
+});
+
+
+app.get("/instructor/:id", (req, res) => {
+    const instructorId = req.params.id;
+    const selectedList = recipes.filter((recipe) => recipe.instructor_id === instructorId);
+    res.send(selectedList);
+});
+
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
